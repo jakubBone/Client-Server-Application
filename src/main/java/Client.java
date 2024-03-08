cae9aae76ab7a7e89bf3cd3499cdafac1e33c172
jakubBone <jakub.bone1990@gmail.com>
2024-03-08 15:01:55 +0100

    Commit 3: Connection handling methods added. Need to eliminate looping print Server response in handleServerCommunication() method in Client class

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,24 +6,58 @@ import java.net.Socket;
 
 public class Client {
     private final int PORT_NUMBER = 5000;
+    Socket clientSocket;
+    PrintWriter outToServer;
+    BufferedReader inFromServer;
+    BufferedReader userInput;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
 
     public Client(){
-        Client client = new Client();
-        client.connectToServer();
+        connectToServer();
+        handleServerCommunication();
     }
 
     public void connectToServer() {
-        try(Socket clientSocket = new Socket("local host", PORT_NUMBER);
-            PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
-
-            System.out.println("Enter \"HELP\" for instruction");
-
+        try {
+            clientSocket = new Socket("localhost", PORT_NUMBER);
+            System.out.println("Connection with Server established");
         } catch (IOException ex){
             ex.printStackTrace();
         }
     }
+    public void handleServerCommunication(){
+        try {
+            outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+            inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Type \"HELP\" to enter COMMANDS MENU: ");
 
+            String request = userInput.readLine().toUpperCase();
+            while(request != null) {
+                outToServer.println(request);
+                String response = inFromServer.readLine();
+                System.out.println("Server response: " + request);
+                if(request.equals("STOP")){
+                    System.out.println("Client disconnected");
+                    disconnect();
+                    break;
+                }
+            }
+        } catch (IOException ex){
+            ex.getStackTrace();
+        }
+    }
 
-
+    public void disconnect() {
+        try {
+            outToServer.close();
+            inFromServer.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,44 +8,55 @@ import java.util.Date;
 
 public class Server {
     private Date serverTimeCreation = new Date();
-    private final int PORT_NUMBER = 5000;
+    private final int PORT = 5000;
     private final String VERSION = "1.0.0";
     private ServerSocket serverSocket;
     private Socket clientSocket;
     BufferedReader inFromClient;
     PrintWriter outToClient;
 
-    private Server() {
+    public static void main(String[] args)  {
+        Server server = new Server();
+    }
+
+    public Server() {
         establishServerConnection();
         handleClientRequest();
     }
 
     public void establishServerConnection() {
         try {
-            serverSocket = new ServerSocket(PORT_NUMBER);
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started");
             clientSocket = serverSocket.accept();
-            System.out.println("Client connected");
+            System.out.println("Connection established");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void handleClientRequest() {
-        inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        outToClient = new PrintWriter((clientSocket.getOutputStream(), true));
 
-        String request = inFromClient.readLine();
-        while(request == null) {
-            System.out.println("CLient reguest: " + request);
-            handleRequest(request, outToClient);
+    public void handleClientRequest(){
+        try{
+            inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToClient = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            String request = inFromClient.readLine();
+            if (request != null) {
+                sendResponse(request, outToClient);
+            }
+            System.out.println("Connection stopped");
+            disconnect();
+        } catch (IOException ex){
+            ex.printStackTrace();
         }
     }
 
-    public void handleRequest(String clientRequest, PrintWriter outToClient){
+    public void sendResponse(String clientRequest, PrintWriter outToClient) throws IOException {
         switch (clientRequest) {
             case "UPTIME":
                 Date currentTime = new Date();
-                System.out.println("Time from server setup: " currentTime.getTime() - serverTimeCreation.getTime());
+                System.out.println("Time from server setup: " + (currentTime.getTime() - serverTimeCreation.getTime()));
                 //outToClient.println(JSON);
                 break;
             case "INFO":
@@ -64,15 +75,19 @@ public class Server {
             case "STOP":
                 System.out.println("Connection stopped");
                 //outToClient.println(JSON);
-                stopConnection();
+                disconnect();
                 break;
         }
     }
-    public void stopConnection() {
-        inFromClient.close();
-        outToClient.close();
-        serverSocket.close();
-        clientSocket.close();
+    public void disconnect()  {
+        try {
+            inFromClient.close();
+            outToClient.close();
+            serverSocket.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
 
