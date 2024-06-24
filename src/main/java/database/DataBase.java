407fcfd0528855da78e85c37c0442b59b2562937
jakubBone <jakub.bone1990@gmail.com>
2024-06-24 13:28:55 +0200

    Commit 116: Rebuilt the project and implemented DSLContext

--- src/main/java/database/DataBase.java
+++ src/main/java/database/DataBase.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import lombok.extern.log4j.Log4j2;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 
 @Log4j2
 public class DataBase {
@@ -14,6 +16,7 @@ public class DataBase {
     private final int PORT_NUMBER = 5432;
     private final String URL = String.format("jdbc:postgresql://localhost:%d/%s", PORT_NUMBER, DATABASE);
     private Connection connection;
+    private DSLContext dslContext;
 
     public DataBase() {
         startConnection();
@@ -21,14 +24,18 @@ public class DataBase {
 
     private void startConnection() {
         try {
+            log.info("Attempting to connect with data base");
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            dslContext = DSL.using(connection);
+
             if(connection != null){
                 log.info("Connection with {} database established on port {}", USER, PORT_NUMBER);
-            } else{
+            } else {
                 log.info("Failed to connect with {} database established on port {}", USER, PORT_NUMBER);
             }
         } catch (SQLException ex) {
             log.error("Error during database connection: {}", ex.getMessage());
+            throw new RuntimeException("Error establishing database connection", ex);
         }
     }
 
