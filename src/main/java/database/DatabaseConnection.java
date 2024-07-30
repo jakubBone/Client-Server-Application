33d4a6c325cc6b6300e1d68502be35ee0dc4cdd3
jakubBone <jakub.bone1990@gmail.com>
2024-07-30 14:31:08 +0200

    Commit 148: Started changing the project database from PostgreSQL to SQLite

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,5 +1,6 @@
 package database;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,16 +9,26 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class DatabaseConnection {
-    private final String USER = "user_manager";
-    private final String PASSWORD = "user123";
-    private final String DATABASE = "user_db";
-    private final int PORT_NUMBER = 5432;
-    private final String URL = String.format("jdbc:postgresql://localhost:%d/%s", PORT_NUMBER, DATABASE);
+    private final String DATABASE_DIRECTORY = "src/main/resources/data";
+    private final String DATABASE = DATABASE_DIRECTORY + "/user_db.sqlite";
+    private final String URL = String.format("jdbc:sqlite:%s", DATABASE);
     private static DatabaseConnection instance;
     private static Connection connection;
 
     public DatabaseConnection() {
-        startConnection();
+        createDatabaseDirectory();
+        connect();
+    }
+
+    private void createDatabaseDirectory() {
+        File directory = new File(DATABASE_DIRECTORY);
+        if (!directory.exists()) {
+            if (directory.mkdirs()) {
+                log.info("Created directory for database at {}", DATABASE_DIRECTORY);
+            } else {
+                log.info("Failed to create directory for database at {}", DATABASE_DIRECTORY);
+            }
+        }
     }
 
     public static synchronized DatabaseConnection getInstance() {
@@ -27,14 +38,14 @@ public class DatabaseConnection {
         return instance;
     }
 
-    public void startConnection() {
+    public void connect() {
         try {
             log.info("Attempting to connect with data base");
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(URL);
             if(connection != null){
-                log.info("Connection with {} database established on port {}", USER, PORT_NUMBER);
+                log.info("Connection with database {} established", DATABASE);
             } else {
-                log.info("Failed to connect with {} database established on port {}", USER, PORT_NUMBER);
+                log.info("Failed to connect with database {}", DATABASE);
             }
         } catch (SQLException ex) {
             log.error("Error during database connection: {}", ex.getMessage());
